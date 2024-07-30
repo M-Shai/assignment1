c3837695b7bcae8a040518d99990204004074249
Say Chaleon Vang <53534907+M-Shai@users.noreply.github.com>
2024-07-30 13:08:12 -0500

    a2 initial commit

---
+++ app/src/main/java/edu/metrostate/myassignment1/HomeScreen.kt
@@ -0,0 +1,100 @@
+package edu.metrostate.myassignment1
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.absoluteOffset
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.SmallFloatingActionButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import org.w3c.dom.Text
+
+@Composable
+fun HomeScreen() : Boolean{
+    var login by remember { mutableStateOf(false) }
+    var register by remember { mutableStateOf(false) }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.LightGray),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(
+            text = "Todo App",
+            fontSize = 48.sp,
+            fontWeight = FontWeight.Bold,
+        )
+
+        Button(onClick = {
+            login = true
+            register = false
+        }) {
+            Text(
+                text = "Login",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold
+            )
+        }
+
+        OutlinedButton(onClick = {
+            register = true
+            login = false
+        }) {
+            Text(
+                text = "Create Account",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold
+            )
+        }
+    }
+    if(login && !register){
+        return login
+    }
+    else
+        return register
+}
+
+fun onClick() {
+    TODO("Not yet implemented")
+}
--- app/src/main/java/edu/metrostate/myassignment1/MainActivity.kt
+++ app/src/main/java/edu/metrostate/myassignment1/MainActivity.kt
@@ -4,23 +4,153 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.ViewModelProvider
-import edu.metrostate.myassignment1.ui.theme.MyAssignment1Theme
+import androidx.navigation.compose.rememberNavController
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val todoViewModel = ViewModelProvider(this)[TodoViewModel::class.java]
+        // val todoViewModel = ViewModelProvider(this)[TodoViewModel::class.java]
         enableEdgeToEdge()
         setContent {
-            MyAssignment1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    TodoView(viewModel = todoViewModel)
+            val navController = rememberNavController()
+            NavHost(navController = navController, startDestination = "Login") {
+                composable("Login") {
+                    Login() {
+                        navController.navigate("Register")
+                        // TodoView(viewModel = todoViewModel)
+                    }
                 }
+                composable("Register") {
+                    Register() {
+                        // TodoView(viewModel = todoViewModel)
+                        navController.navigate("Todo")
+                    }
+                }
+                /*
+                composable("Todo") {
+                    Todo(){
+                        TodoView(viewModel = todoViewModel)
+                    }
+                }
+                */
+
             }
+        }
+
+    }
+}
+
+@Composable
+fun Login(
+    onLoginButtonClicked: () -> Unit
+){
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Cyan),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(
+            text = "Login",
+            fontSize = 48.sp,
+            fontWeight = FontWeight.Bold,
+        )
+        Button(onClick = onLoginButtonClicked) {
+            Text(
+                text = "Login",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold
+            )
+        }
+        /*
+        Button(onClick = onRegButtonClicked) {
+            Text(
+                text = "Register new user",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold
+            )
+        }
+        */
+
+    }
+
+}
+
+@Composable
+fun Register(
+    onButtonClicked: () -> Unit
+){
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Cyan),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(
+            text = "Register",
+            fontSize = 48.sp,
+            fontWeight = FontWeight.Bold,
+        )
+        Button(onClick = onButtonClicked) {
+            Text(
+                text = "Register new user",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold
+            )
+        }
+        Button(onClick = onButtonClicked) {
+            Text(
+                text = "Login",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold
+            )
+        }
+    }
+}
+
+@Composable
+fun Todo(
+    // onTodoButtonClicked: @Composable () -> Unit
+) {
+    var showTodo: Boolean = false
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Cyan),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(
+            text = "Todo",
+            fontSize = 48.sp,
+            fontWeight = FontWeight.Bold,
+        )
+        Button(onClick = {
+            TodoView(viewModel = todoViewModel)
+        }) {
+            Text(
+                text = "Start Todo",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold
+            )
         }
     }
 }
