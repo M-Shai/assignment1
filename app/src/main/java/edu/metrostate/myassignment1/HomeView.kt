198da72977c1a556a4cfac0bb39062878744a85a
Say Chaleon Vang <53534907+M-Shai@users.noreply.github.com>
2024-07-31 15:07:56 -0500

    a2 Navigation works, Login works, Register new user works, Todo List works

--- app/src/main/java/edu/metrostate/myassignment1/HomeView.kt
+++ app/src/main/java/edu/metrostate/myassignment1/HomeView.kt
@@ -3,11 +3,14 @@ package edu.metrostate.myassignment1
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.absoluteOffset
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -17,6 +20,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -29,6 +33,9 @@ import androidx.navigation.compose.rememberNavController
 fun HomeView(viewModel: HomeViewModel, navController: NavController){
     var login by remember { mutableStateOf(false) }
     var register by remember { mutableStateOf(false) }
+    var inputEmail by remember{ mutableStateOf("") }
+    var inputPassword by remember { mutableStateOf("") }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -36,7 +43,29 @@ fun HomeView(viewModel: HomeViewModel, navController: NavController){
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
+        OutlinedTextField(
+            modifier = Modifier
+                .background(Color.Transparent)
+                .padding(50.dp, 4.dp)
+                .fillMaxWidth(),
+            value = inputEmail,
+            onValueChange = { inputEmail = it },
+            label = {Text(text = stringResource(id = R.string.email))}
+        )
+        OutlinedTextField(
+            modifier = Modifier
+                .background(Color.Transparent)
+                .padding(50.dp, 4.dp)
+                .fillMaxWidth(),
+            value = inputEmail,
+            onValueChange = { inputPassword = it },
+            label = {Text(text = stringResource(id = R.string.password))}
+        )
+
         Text(
+            modifier = Modifier
+                .absoluteOffset(0.dp, (-344).dp),
+            color = Color.Blue,
             text = "Todo App",
             fontSize = 48.sp,
             fontWeight = FontWeight.Bold,
@@ -44,6 +73,7 @@ fun HomeView(viewModel: HomeViewModel, navController: NavController){
 
         Button(
             modifier = Modifier
+                .offset(0.dp,0.dp)
                 .padding(2.dp)
                 .fillMaxWidth(),
             onClick = {
@@ -56,7 +86,10 @@ fun HomeView(viewModel: HomeViewModel, navController: NavController){
             )
         }
 
-        OutlinedButton(onClick = {
+        OutlinedButton(
+            modifier = Modifier
+                .offset(0.dp,30.dp),
+            onClick = {
             navController.navigate(Screen.Register.route)
         }) {
             Text(
--- app/src/main/java/edu/metrostate/myassignment1/RegisterView.kt
+++ app/src/main/java/edu/metrostate/myassignment1/RegisterView.kt
@@ -4,9 +4,14 @@ package edu.metrostate.myassignment1
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.absoluteOffset
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -16,8 +21,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModel
 import androidx.navigation.NavController
@@ -27,6 +34,12 @@ import androidx.navigation.compose.rememberNavController
 fun RegisterView(viewModel: RegisterViewModel, navController : NavController){
     var login by remember { mutableStateOf(false) }
     var register by remember { mutableStateOf(false) }
+    var inputEmail by remember{ mutableStateOf("") }
+    var inputPassword by remember { mutableStateOf("") }
+    var inputFirstName by remember{ mutableStateOf("") }
+    var inputLastName by remember { mutableStateOf("") }
+    var inputPhoneNumber by remember{ mutableStateOf("") }
+    var inputAddress by remember { mutableStateOf("") }
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -35,10 +48,60 @@ fun RegisterView(viewModel: RegisterViewModel, navController : NavController){
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
         Text(
+            modifier = Modifier
+                .absoluteOffset(0.dp, (-135).dp),
+            color = Color.Blue,
             text = "Todo App",
             fontSize = 48.sp,
             fontWeight = FontWeight.Bold,
         )
+        Text(
+            modifier = Modifier
+                .absoluteOffset(0.dp, (-0).dp),
+            color = Color.Blue,
+            text = "Enter New User Information",
+            fontSize = 22.sp,
+            fontWeight = FontWeight.Bold,
+        )
+
+        OutlinedTextField(
+            modifier = Modifier
+                .background(Color.Transparent)
+                .padding(50.dp, 4.dp)
+                .fillMaxWidth(),
+            value = inputFirstName,
+            onValueChange = { inputFirstName = it },
+            label = {Text(text = stringResource(id = R.string.fname))}
+        )
+        OutlinedTextField(
+            modifier = Modifier
+                .background(Color.Transparent)
+                .padding(50.dp, 4.dp)
+                .fillMaxWidth(),
+            value = inputLastName,
+            onValueChange = { inputLastName = it },
+            label = {Text(text = stringResource(id = R.string.lname))}
+        )
+        OutlinedTextField(
+            modifier = Modifier
+                .background(Color.Transparent)
+                .padding(50.dp, 4.dp)
+                .fillMaxWidth(),
+            value = inputEmail,
+            onValueChange = { inputEmail = it },
+            label = {Text(text = stringResource(id = R.string.email))}
+        )
+        OutlinedTextField(
+            modifier = Modifier
+                .background(Color.Transparent)
+                .padding(50.dp, 4.dp)
+                .fillMaxWidth(),
+            value = inputEmail,
+            onValueChange = { inputPassword = it },
+            label = {Text(text = stringResource(id = R.string.password))}
+        )
+
+        Spacer(modifier = Modifier.padding(6.dp))
         Button(onClick = {
             navController.navigate(Screen.Home.route)
         }) {
